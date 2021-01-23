b9238076a561c4279970d280f93ef12b0cc6bdcd
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-01-23 02:57:04 -0800

    complete substraction function

--- Calculator/assets/js/app.js
+++ Calculator/assets/js/app.js
@@ -12,16 +12,30 @@ let result={
 //ask which operator the user want
 result.res=parseInt(window.prompt("Choose your operator \n For addition inter 1 \n For substraction inter 2 \n For multiplication inter 3 \n For division inter 4"))
 function chooceNumber(){
-    if (result.res == 1){
-         add();
-}}
+if (result.res == 1){
+    add();
+}
+else if(result.res == 2){
+    sub();
+}
+
+}
 function add(){
-    let numberOfAdd = parseInt(window.prompt("How many number do you want to add?"));
+    let numberOfAdd = parseInt(prompt("How many number do you want to add?"));
     for (let i = 0; i < numberOfAdd; i++) {
         result.numberAdd[i] = parseInt(prompt((i + 1) + " number you want to add"));
         result.sum = result.sum + result.numberAdd[i];
     }
     console.log("sum = " + result.sum);
+
+}
+function sub(){
+    let firstNumber = parseInt(prompt("inter the first number"));
+    let secondNumber = parseInt(prompt("inter the second number"))
+    
+    result.dif = firstNumber - secondNumber;
+        
+    console.log("difference = " + result.dif);
 
 }
 chooceNumber();
